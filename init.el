a529f6520f8109c4755498a7a37a33612e86a48c
Espen Volden <espenv@vg.no>
2012-08-15 13:09:52 +0200

    Initial import

---
+++ init.el
@@ -0,0 +1,96 @@
+;; Load path etc.
+(setq dotfiles-dir (file-name-directory
+                    (or (buffer-file-name) load-file-name)))
+
+(add-to-list 'load-path dotfiles-dir)
+(add-to-list 'load-path "~/.emacs.d/vendor/")
+(add-to-list 'load-path "~/.emacs.d/vendor/phpplus-mode")
+
+;; ELPA
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                         ("marmalade" . "http://marmalade-repo.org/packages")
+                         ("melpa" . "http://melpa.milkbox.net/packages/")))
+
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+(ansi-color-for-comint-mode-on)
+
+;; Disable startup screen
+(setq inhibit-startup-message t)
+
+(setq xterm-mouse-mode t)
+
+;; Disable menu
+(menu-bar-mode -1)
+
+;; Transparently open compressed files
+(auto-compression-mode t)
+
+;; Save a list of recent files visited.
+(recentf-mode 1)
+
+;; Highlight matching parentheses when the point is on them.
+(show-paren-mode 1)
+
+;; enable column-number-mode
+(column-number-mode t)
+
+;; Ido
+(ido-mode t)
+(setq ido-enable-prefix nil
+      ido-enable-flex-matching t
+      ido-create-new-buffer 'always
+      ido-use-filename-at-point 'guess
+      ido-max-prospects 10)
+
+(set-default 'indent-tabs-mode nil)
+(set-default 'indicate-empty-lines t)
+(set-default 'imenu-auto-rescan t)
+
+;; Uniquify
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'forward)
+
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+(random t) ;; Seed the random-number generator
+
+;; Don't clutter up directories with files~
+(setq backup-directory-alist `(("." . ,(expand-file-name
+                                        (concat dotfiles-dir "backups")))))
+
+(add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
+
+;; Default to unified diffs
+(setq diff-switches "-u -w")
+
+(eval-after-load 'diff-mode
+  '(progn
+     (set-face-foreground 'diff-added "green4")
+     (set-face-foreground 'diff-removed "red3")))
+
+(eval-after-load 'magit
+  '(progn
+     (set-face-foreground 'magit-diff-add "green3")
+     (set-face-foreground 'magit-diff-del "red3")))
+
+;; ;; PHP-mode
+;; (add-to-list 'auto-mode-alist '("\\.php" . php+-mode))
+
+;; After elpa load
+(defun my-after-init ()
+  (require 'twilight-theme))
+(add-hook 'after-init-hook 'my-after-init)
+
+(font-lock-add-keywords
+ 'js2-mode `(("\\(function *\\)("
+              (0 (progn (compose-region (match-beginning 1)
+                                        (match-end 1) "f")
+                        nil)))))
+
+
+;; 
+(require 'key-bindings)
+(require 'misc-func)
+(require 'setup-php)

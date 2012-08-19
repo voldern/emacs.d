f3d67861da52c3f2efe0c0a91fd463e1311bc311
Espen Volden <voldern@web01.responsreklame.no>
2012-08-19 18:36:30 +0200

    Install missing packages automaticly

--- init.el
+++ init.el
@@ -7,9 +7,10 @@
 (add-to-list 'load-path "~/.emacs.d/vendor/phpplus-mode")
 
 ;; ELPA
-(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                         ("marmalade" . "http://marmalade-repo.org/packages")
-                         ("melpa" . "http://melpa.milkbox.net/packages/")))
+(require 'setup-packages)
+;; (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+;;                          ("marmalade" . "http://marmalade-repo.org/packages")
+;;                          ("melpa" . "http://melpa.milkbox.net/packages/")))
 
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
---
+++ setup-packages.el
@@ -0,0 +1,20 @@
+(setq package-list '(php-mode php+-mode multi-web-mode haml-mode yaml-mode magit
+                              flymake-php flymake-ruby ace-jump-mode))
+
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                         ("marmalade" . "http://marmalade-repo.org/packages")
+                         ("melpa" . "http://melpa.milkbox.net/packages/")))
+
+; activate all the packages (in particular autoloads)
+(package-initialize)
+
+; fetch the list of packages available 
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+; install the missing packages
+(dolist (package package-list)
+  (when (not (package-installed-p package))
+    (package-install package)))
+
+(provide 'setup-packages)

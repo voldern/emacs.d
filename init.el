5b6bb3e3a7ca9798a4d097f4099f7b8b1bd13944
Espen Volden <espenv@vg.no>
2013-05-27 14:45:34 +0200

    Keep region when undoing in region with tree-undo

--- init.el
+++ init.el
@@ -91,6 +91,18 @@
 (require 'undo-tree)
 (global-undo-tree-mode)
 
+;; Keep region when undoing in region
+(defadvice undo-tree-undo (around keep-region activate)
+  (if (use-region-p)
+      (let ((m (set-marker (make-marker) (mark)))
+            (p (set-marker (make-marker) (point))))
+        ad-do-it
+        (goto-char p)
+        (set-mark m)
+        (set-marker p nil)
+        (set-marker m nil))
+    ad-do-it))
+
 ;; After elpa load
 (defun my-after-init ()
   (require 'twilight-theme))

32421a034cfb96eade1005e9606b0b2f547a6b0d
Espen Volden <espenv@vg.no>
2013-06-13 15:45:43 +0200

    Provide setup-html

--- setup-html.el
+++ setup-html.el
@@ -4,3 +4,5 @@
      (define-key html-mode-map (kbd "C-c C-w") 'html-wrap-in-tag)
      (tagedit-add-experimental-features)
      (add-hook 'html-mode-hook (lambda () (tagedit-mode 1)))))
+
+(provide 'setup-html)

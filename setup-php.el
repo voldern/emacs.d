db9ca9af9ca7ef9f18d0efa405c6db0595c89d0a
Espen Volden <espenv@vg.no>
2014-01-21 16:07:01 +0100

    Fix phpmd and phpcs flycheck

--- setup-php.el
+++ setup-php.el
@@ -3,12 +3,16 @@
 ;;; Commentary:
 
 ;;; Code:
+(eval-when-compile
+  (require 'flycheck)
+  (require 'php-electric))
+
 (defun setup-multi-web-mode ()
   "Function to setup multi-web-mode."
   (require 'php-mode)
   (require 'php+-mode)
   (php+-mode-setup)
-  
+
   (require 'multi-web-mode)
   (setq mweb-default-major-mode 'html-mode)
   (setq mweb-tags '((php-mode "<\\?php\\|<\\? \\|<\\?=" "\\?>")
@@ -18,20 +22,23 @@
   (multi-web-global-mode 1))
 
 (add-hook 'after-init-hook 'setup-multi-web-mode)
+
 (add-hook 'php+-mode-hook (lambda ()
                             (require 'php-electric)
-                            (php-electric-mode)))
+                            (php-electric-mode)
+                            (setq-local flycheck-phpcs-standard "VG")
+                            (setq-local flycheck-phpmd-rulesets '("codesize" "design" "naming" "unusedcode"))
+                            (flycheck-mode 1)))
 
-(flycheck-declare-checker flycheck-checker-php+
+(flycheck-define-checker flycheck-checker-php+
   "PHP+ mode flycheck"
-  :command '("php" "-l" "-d" "error_reporting=E_ALL" "-d" "display_errors=1" "-d" "log_errors=0" source)
-  :error-patterns '(("\\(?:Parse\\|Fatal\\|syntax\\) error[:,] \\(.*\\) in \\(.*\\) on line \\([0-9]+\\)" error))
-  :modes 'php+-mode)
+  :command ("php" "-l" "-d" "error_reporting=E_ALL" "-d" "display_errors=1" "-d" "log_errors=0" source)
+  :error-patterns ((error "\\(?:Parse\\|Fatal\\|syntax\\) error[:,] \\(.*\\) in \\(.*\\) on line \\([0-9]+\\)"))
+  :modes php+-mode)
 
 (defun my-after-init-php ()
   "After php init hook."
-  (require 'flycheck)
-  (add-to-list 'flycheck-checkers 'flycheck-checker-php+))
+  (require 'flycheck))
 
 (add-hook 'after-init-hook 'my-after-init-php)
 
@@ -53,20 +60,21 @@
                       buffer)))
 (add-hook 'compilation-finish-functions 'bury-compile-buffer-if-successful)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(php+-mode-delete-trailing-whitespace t)
- '(php+-mode-php-compile-on-save t)
- '(php+-mode-show-project-in-modeline t)
- '(php+-mode-show-trailing-whitespace t)
- '(php-doc-default-author (quote ("Espen Volden" . "voldern@hoeggen.net")))
- '(php-file-patterns (quote ("\\.php[s345t]?\\'" "\\.inc\\'")))
- '(php-html-basic-offset 4)
- '(php-project-list (quote (("direkte" "~/webdev/html/livestudio" "~/webdev/html/livestudio/TAGS" nil "" nil (("" . "") "" "" "" "" "" "" "" "") "Livestudio" ""))))
- '(phpcs-standard "VG"))
+(setq phpcs-standard "VG")
+
+;; (custom-set-variables
+;;  ;; custom-set-variables was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  '(php+-mode-delete-trailing-whitespace t)
+;;  '(php+-mode-php-compile-on-save t)
+;;  '(php+-mode-show-project-in-modeline t)
+;;  '(php+-mode-show-trailing-whitespace t)
+;;  '(php-doc-default-author (quote ("Espen Volden" . "voldern@hoeggen.net")))
+;;  '(php-file-patterns (quote ("\\.php[s345t]?\\'" "\\.inc\\'")))
+;;  '(php-html-basic-offset 4)
+;;  '(php-project-list (quote (("direkte" "~/webdev/html/livestudio" "~/webdev/html/livestudio/TAGS" nil "" nil (("" . "") "" "" "" "" "" "" "" "") "Livestudio" "")))))
 
 (add-hook 'html-mode-hook
           (lambda ()

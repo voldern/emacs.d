4ebe5df05b0cab5822063fb8e32f746d39533235
Espen Volden <voldern@hoeggen.net>
2014-04-09 16:26:29 +0200

    Support HTML5 script and style tags without type in multi-web-mode

--- setup-php.el
+++ setup-php.el
@@ -16,8 +16,8 @@
   (require 'multi-web-mode)
   (setq mweb-default-major-mode 'html-mode)
   (setq mweb-tags '((php-mode "<\\?php\\|<\\? \\|<\\?=" "\\?>")
-                    (js-mode "<script +\\(type=\"text/javascript\"\\|language=\"javascript\"\\)[^>]*>" "</script>")
-                    (css-mode "<style +type=\"text/css\"[^>]*>" "</style>")))
+                    (js-mode "<script\\( +\\(type=\"text/javascript\"\\|language=\"javascript\"\\)\\)?[^>]*>" "</script>")
+                    (css-mode "<style\\( +\\type=\"text/css\"\\)?[^>]*>" "</style>")))
   (setq mweb-filename-extensions '("htm" "html" "ctp" "phtml"))
   (multi-web-global-mode 1))
 

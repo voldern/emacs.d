6f26f9449cd3aea5d86f5d600a31fd651430fafb
Espen Volden <voldern@hoeggen.net>
2014-09-10 15:46:36 +0200

    Move helm keybidings to key-bindings.el

--- init.el
+++ init.el
@@ -163,10 +163,6 @@
 
 ;; helm
 (helm-mode 1)
-(global-set-key (kbd "M-x") 'helm-M-x)
-(global-set-key (kbd "M-y") 'helm-show-kill-ring)
-(global-set-key (kbd "C-x b") 'helm-mini)
-(global-set-key (kbd "C-x C-f") 'helm-find-files)
 
 ;; projectile
 (projectile-global-mode)
--- key-bindings.el
+++ key-bindings.el
@@ -53,4 +53,9 @@
 
 (define-key yas-minor-mode-map (kbd "<backtab>") 'yas-expand)
 
+(global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "M-y") 'helm-show-kill-ring)
+(global-set-key (kbd "C-x b") 'helm-mini)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+
 (provide 'key-bindings)

4fa421c760b98f90e941b6098b8e3fbf716e4703
Espen Volden <voldern@hoeggen.net>
2014-11-20 10:24:31 +0100

    Change font to Liberation Sans

--- init.el
+++ init.el
@@ -14,7 +14,11 @@
   (add-to-list 'default-frame-alist '(font .  "Envy Code R-12")))
 
 (custom-set-faces
-  '(variable-pitch ((t (:family "Liberation Sans")))))
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(variable-pitch ((t (:family "Liberation Sans")))))
 
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
@@ -230,10 +234,3 @@
  '(php-html-basic-offset 4)
  '(php-project-list (quote (("direkte" "~/webdev/html/livestudio" "~/webdev/html/livestudio/TAGS" nil "" nil (("" . "") "" "" "" "" "" "" "" "") "Livestudio" ""))))
  '(phpcs-standard "VG"))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
-
- )

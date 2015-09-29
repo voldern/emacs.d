64556b6f454ebc3d090cf0e3238254c2ed86a4f7
Espen Volden <voldern@hoeggen.net>
2015-09-30 00:01:34 +0200

    Add support for JSX

--- lisp/setup-web.el
+++ lisp/setup-web.el
@@ -22,4 +22,12 @@
 (add-hook 'css-mode-hook 'syntax-color-hex)
 (add-hook 'scss-mode-hook 'syntax-color-hex)
 
+(add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
+
+(defun my-web-mode-hook ()
+  "Hooks for Web mode."
+  (setq web-mode-markup-indent-offset 4))
+(add-hook 'web-mode-hook  'my-web-mode-hook)
+
+
 (provide 'setup-web)

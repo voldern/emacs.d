9888d1d2a716a9dcb1c7b8a27856a97e844c8923
Espen Volden <voldern@hoeggen.net>
2016-04-19 14:29:02 +0200

    Load js2-jsx-mode in files with .jsx extension

--- lisp/setup-js.el
+++ lisp/setup-js.el
@@ -30,7 +30,7 @@
 
 ;;;;; js2-mode ;;;;;
 
-(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-jsx-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx\\'" . js2-jsx-mode))
 (add-to-list 'interpreter-mode-alist '("node" . js2-jsx-mode))
 
 ;; Turn function() into f() in js2-mode
